32b9d1f4a0f6a5337149dc9581d7fb44d2bae446
jacques troussard <jacques.troussard@gmail.com>
2024-02-15 15:04:24 -0500

    makes tests package and fixes endpoint method names

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def landing_page():
+def home():
     return "<h1>This is my example Flask app</h1>"
 
 @app.route('/about')
-def landing_page():
+def about_page():
     return "<h1>This is a very interesting ABOUT page</h1>"
 
 @app.route('/about/v2')
-def landing_page():
+def about_v2_page():
     return "<h1>This is a very interesting ABOUT page?</h1>"
 
 @app.route('/hello/<name>')
